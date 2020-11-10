156692be98b9d209a046cb46728ea0f398ef7cb9
Pathfinder - Chris Barlow <pathfindercb@users.noreply.github.com>
2020-11-10 11:51:54 -0500

    Add files via upload

---
+++ private/Wtrak/DBconnect - create your own.php
@@ -0,0 +1,8 @@
+<?php
+	//these are the variables for the db in separate file for security
+	$dbtype = 'sqlite'; // or 'mysql'
+	$host = 'localhost';
+	$db   = 'dbname';
+	$user = 'dbuser';
+	$pass = 'userpwassword';
+?>
---
+++ private/Wtrak/DBkey - create your own.php
@@ -0,0 +1,5 @@
+<?php
+	//this is the variables for the crypt key in separate file for security
+	$COBkey = '---create a 32 character key ---';
+	$COBnonce = 'create16character';
+?>
